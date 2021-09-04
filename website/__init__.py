5365bae1ca30fb2f147a9b3bfdc0bfffa6d71a31
Nolan Roth <nolanrroth12@gmail.com>
2021-09-04 15:49:23 -0700

    Updated postgres prod uri.

--- website/__init__.py
+++ website/__init__.py
@@ -12,14 +12,14 @@ app.config['SECRET_KEY'] = 'ee0442debd6e5574666cd67a'
 app.jinja_options['extensions'].append('jinja2.ext.do')
 
 # change this to dev to use development database and prod to use production database
-ENV = 'proc'
+ENV = 'prod'
 
 if ENV == 'dev':
 	app.debug = True
 	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%40Vcs39706@localhost/rankings'
 else:
 	app.debug = False
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ejwlsaknequdfu:a354bde5759165e426c0c8d2cc94fd12fb29bc9b65ed4f5eda9b6027c9377058@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d8sj9c085m0joh'
+	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ejwlsaknequdfu:a354bde5759165e426c0c8d2cc94fd12fb29bc9b65ed4f5eda9b6027c9377058@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d8sj9c085m0joh'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -29,4 +29,4 @@ login_manager.login_view = "login_page"
 login_manager.login_message_category = "info"
 
 # Comment out when you want to run build_database.py or you will get errors
-from website import routes
+#from website import routes
