78d93aba607c0c6d95bf11cc06e7340462895027
Nolan Roth <nolanrroth12@gmail.com>
2021-09-04 23:03:01 -0700

    New chrome options again.

--- build_database.py
+++ build_database.py
@@ -18,8 +18,7 @@ def scrape():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-notifications")
 
     #driver = webdriver.Chrome(executable_path=os.path.join(os.path.dirname(os.getcwd()), "chromedriver.exe"))
     driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
