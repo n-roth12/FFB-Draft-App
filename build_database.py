5db3ee804f26c1a7d92718ce034d8e9d89ca745a
Nolan Roth <nolanrroth12@gmail.com>
2021-09-04 22:49:21 -0700

    Fixed type on environment.

--- build_database.py
+++ build_database.py
@@ -16,13 +16,13 @@ def build():
 
 def scrape():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.binary_location = os.environment.get("GOOGLE_CHROME_BIN")
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--no-sandbox")
 
     #driver = webdriver.Chrome(executable_path=os.path.join(os.path.dirname(os.getcwd()), "chromedriver.exe"))
-    driver = webdriver.Chrome(executable_path=os.environment.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
     fp_scrape(opts, driver)
     sport_news_scrape(opts, driver)
