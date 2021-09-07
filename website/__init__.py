f621010588a7488de035f560aea9d6d0cb3b5b89
Nolan Roth <nolanrroth12@gmail.com>
2021-09-06 17:46:39 -0700

    Website working.

--- website/__init__.py
+++ website/__init__.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 app = Flask(__name__, static_folder=os.path.abspath('/Users/NolanRoth/Desktop/ProjectWebsite'))
-app.config['SECRET_KEY'] = 'ee0442debd6e5574666cd67a'
+app.config['SECRET_KEY'] = '*redacted*'
 app.jinja_options['extensions'].append('jinja2.ext.do')
 
 # change this to dev to use development database and prod to use production database
@@ -16,10 +16,10 @@ ENV = 'prod'
 
 if ENV == 'dev':
 	app.debug = True
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%40Vcs39706@localhost/rankings'
+	app.config['SQLALCHEMY_DATABASE_URI'] = '*redacted*'
 else:
 	app.debug = False
-	app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://ejwlsaknequdfu:a354bde5759165e426c0c8d2cc94fd12fb29bc9b65ed4f5eda9b6027c9377058@ec2-44-194-145-230.compute-1.amazonaws.com:5432/d8sj9c085m0joh'
+	app.config['SQLALCHEMY_DATABASE_URI'] = '*redacted*'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
