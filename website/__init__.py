dd3784d1c9065b82dc9dfe92e5e4d69f8479e3c3
Nolan Roth <nolanrroth12@gmail.com>
2021-10-13 22:40:27 -0700

    Now using try/except block to determine if build is prod or dev.

--- website/__init__.py
+++ website/__init__.py
@@ -10,16 +10,13 @@ from selenium.webdriver.chrome.options import Options
 app = Flask(__name__, static_folder=os.path.abspath('/Users/NolanRoth/Desktop/ProjectWebsite'))
 app.jinja_options['extensions'].append('jinja2.ext.do')
 
-# change this to dev to use development database and prod to use production database
-is_prod = os.environ.get('IS_HEROKU', None)
-
-if is_prod:
+try:
+	import config
+except ModuleNotFoundError:
 	app.debug = False
 	app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('PROD_DATABASE_URI')
-	app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')
+	app.config['SECRET_KEY'] = os.environ.get('APP_SECRET_KEY')	
 else:
-	app.debug = True
-	import config
 	app.config['SQLALCHEMY_DATABASE_URI'] = config.dev_database_uri
 	app.config['SECRET_KEY'] = config.app_secret_key
 
